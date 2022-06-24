d4f70b280f16ee4782e384d41fb88d54fcff0bf5
mawais54013 <mawais54013@gmail.com>
2022-06-23 22:05:22 -0700

    Added Contour

--- motion_detector.py
+++ motion_detector.py
@@ -24,11 +24,16 @@ while True:
 
     (cnts,_)  =cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
-    
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
+            continue
+        (x, y, w, h)=cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
 
     cv2.imshow("Gray Frame",gray)
     cv2.imshow("Delta Frame",delta_frame)
     cv2.imshow("Threshold Frame", thresh_frame)
+    cv2.imshow("Color Frame", frame)
 
     key=cv2.waitKey(1)
     print(gray)
