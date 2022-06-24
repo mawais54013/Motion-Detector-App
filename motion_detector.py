5aa6cbf3301d8eb0863ee291ab2e849049fbb54a
mawais54013 <mawais54013@gmail.com>
2022-06-23 21:48:58 -0700

    added delta frame

--- motion_detector.py
+++ motion_detector.py
@@ -1,5 +1,7 @@
 import cv2, time
 
+first_frame=None
+
 video=cv2.VideoCapture(1)
 # Might need to change above to 0 if you don't have obs installed
 
@@ -10,11 +12,20 @@ while True:
     check, frame = video.read()
 
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray=cv2.GaussianBlur(gray,(21,21),0)
 
-    cv2.imshow("Capturing",gray)
+    if first_frame is None:
+        first_frame = gray
+        continue
+
+    delta_frame=cv2.absdiff(first_frame,gray)
+
+    cv2.imshow("Gray Frame",gray)
+    cv2.imshow("Delta Frame",delta_frame)
 
     key=cv2.waitKey(1)
     print(gray)
+    print(delta_frame)
 
     if key==ord('q'):
         break
