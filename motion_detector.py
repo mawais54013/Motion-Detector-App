e9dae4da1e2fc5e0c9c7c237f215c7704391e7be
mawais54013 <mawais54013@gmail.com>
2022-06-24 22:06:33 -0700

    Added Times

--- motion_detector.py
+++ motion_detector.py
@@ -1,15 +1,16 @@
+from datetime import datetime
 import cv2, time
 
 first_frame=None
+status_list=[]
+times=[]
 
 video=cv2.VideoCapture(1)
 # Might need to change above to 0 if you don't have obs installed
 
-a=0
-
 while True:
-    a=a+1
     check, frame = video.read()
+    status=0
 
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     gray=cv2.GaussianBlur(gray,(21,21),0)
@@ -22,13 +23,23 @@ while True:
     thresh_frame=cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     thresh_frame=cv2.dilate(thresh_frame, None, iterations=2)
 
-    (cnts,_)  =cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    (_,cnts,_)=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
-            continue
+        if cv2.contourArea(contour) < 10000:
+            continue      
+        status=1
+
         (x, y, w, h)=cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
+    status_list.append(status)
+
+
+    if status_list[-1] == 1 and status_list[-2] == 0:
+        times.append(datetime.now())
+    if status_list[-1] == 0 and status_list[-2] == 1:
+        times.append(datetime.now())
+
 
     cv2.imshow("Gray Frame",gray)
     cv2.imshow("Delta Frame",delta_frame)
@@ -36,11 +47,11 @@ while True:
     cv2.imshow("Color Frame", frame)
 
     key=cv2.waitKey(1)
-    print(gray)
-    print(delta_frame)
 
     if key==ord('q'):
         break
-print(a)
+    
+    print(status_list)
+
 video.release()
 cv2.destroyAllWindows
